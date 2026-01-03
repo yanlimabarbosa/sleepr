033b307336c6f80626b143144bae07dc0f28cd57
Yan Lima Barbosa <yanbr763@gmail.com>
2026-01-03 19:58:27 +0000

    feat: implement Auth module

---
+++ apps/auth/src/users/users.repository.ts
@@ -0,0 +1,16 @@
+import { AbstractRepository } from "@app/common";
+import { UserDocument } from "./models/user.schema";
+import { Injectable, Logger } from "@nestjs/common";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+
+@Injectable()
+export class UsersRepository extends AbstractRepository<UserDocument> {
+  protected readonly logger = new Logger(UsersRepository.name);
+
+constructor(
+    @InjectModel(UserDocument.name) userModel: Model<UserDocument>,
+  ) {
+    super(userModel);
+  }
+}

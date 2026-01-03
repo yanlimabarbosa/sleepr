53c44fe7b16017e99b4e79904e80db8f3cf49381
Yan Lima Barbosa <yanbr763@gmail.com>
2026-01-03 03:36:09 -0300

    feat: integrate logging in Reservations module
    Added global validation pipe and integrated Pino logger for enhanced logging capabilities in the Reservations module. Introduced LoggerModule for centralized logging configuration and updated the module imports accordingly.

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger as PinoLogger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(PinoLogger));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,9 +4,14 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common/database';
 import { ReservationRepository } from './reservation.repository';
 import { ReservationDocument, ReservationSchema } from './reservations/models/reservation.schema';
+import { LoggerModule } from '@app/common/logger';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
+    LoggerModule,
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
 })

506f5ab4bd0bd63bb9765ebbcae5546027e52af1
Yan Lima Barbosa <yanbr763@gmail.com>
2026-08-25 21:41:24 -0300

    feat(auth): hash passwords and protect user routes
    - Hash passwords with argon2 and validate credentials on login
    - Reject duplicate emails on create
    - Protect get-user route with JwtAuthGuard and @CurrentUser

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,51 @@
-import { Body, Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import * as argon2 from 'argon2';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async create(@Body() createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+  async create(createUserDto: CreateUserDto) {
+    await this.validateCreateUserDto(createUserDto);
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await argon2.hash(createUserDto.password),
+    });
+  }
+
+  private async validateCreateUserDto({ email }: CreateUserDto) {
+    const existing = await this.usersRepository
+      .findOne({ email })
+      .catch(() => null);
+
+    if (existing) {
+      throw new UnprocessableEntityException('Email already exists');
+    }
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository
+      .findOne({ email })
+      .catch(() => null);
+
+    const credentialsValid =
+      user && (await argon2.verify(user.password, password));
+
+    if (!credentialsValid) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+
+    return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.usersRepository.findOne(getUserDto);
   }
 }
